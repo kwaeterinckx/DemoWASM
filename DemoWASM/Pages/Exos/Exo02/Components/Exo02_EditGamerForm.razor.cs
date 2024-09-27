cc3b86f91e945cc9a6e78e9c63a604b06d4b9712
kwaeterinckx <159497772+kwaeterinckx@users.noreply.github.com>
2024-09-27 15:45:14 +0200

    Add edit an details

---
+++ DemoWASM/Pages/Exos/Exo02/Components/Exo02_EditGamerForm.razor.cs
@@ -0,0 +1,20 @@
+﻿using DemoWASM.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DemoWASM.Pages.Exos.Exo02.Components
+{
+    public partial class Exo02_EditGamerForm
+    {
+        [Parameter]
+        public Exo02_Gamer EditGamerForm { get; set; }
+
+        [Parameter]
+        public EventCallback<Exo02_Gamer> ReponseEvent { get; set; }
+
+        public void EditGamer()
+        {
+            ReponseEvent.InvokeAsync(EditGamerForm);
+        }
+    }
+
+}
--- DemoWASM/Pages/Exos/Exo02/Exo02.razor.cs
+++ DemoWASM/Pages/Exos/Exo02/Exo02.razor.cs
@@ -13,30 +13,22 @@ namespace DemoWASM.Pages.Exos.Exo02
         public int? gamerId { get; set; }
 
         public Exo02_Gamer GamerForm { get; set; }
-        public List<Exo02_Gamer> Gamers { get; set; }
+        public Dictionary<int, Exo02_Gamer> Gamers { get; set; }
 
         public Exo02()
         {
-            Gamers = new List<Exo02_Gamer>();
-            Gamers.Add(new Exo02_Gamer()
-            {
-                Pseudo = "Kévin",
-                Email = "Waeterinckx",
-                Password = "Test123456",
-                DateNaissance = DateOnly.Parse("1983-03-11")
-            });
+            Gamers = new Dictionary<int, Exo02_Gamer>();
             GamerForm = new Exo02_Gamer();
         }
 
         public void SubmitAddForm()
         {
-            Gamers.Add(GamerForm);
+            Gamers.Add(GamerForm.Id, GamerForm);
             GamerForm = new Exo02_Gamer();
         }
-        public void SubmitEditForm()
+        public void SubmitEditForm(Exo02_Gamer gamer)
         {
-            Gamers.Add(GamerForm);
-            GamerForm = new Exo02_Gamer();
+            Gamers[(int)gamerId] = gamer;
         }
     }
 }
